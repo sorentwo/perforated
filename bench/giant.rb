050b930985f4d80fc31901ec2d5e6a367d1397e9
Parker Selbert <parker@sorentwo.com>
2014-10-08 16:03:36 -0500

    Stop benchmarking dalli, redis is enough

--- bench/giant.rb
+++ bench/giant.rb
@@ -3,10 +3,8 @@ require 'bundler'
 Bundler.setup
 
 require 'benchmark'
-require 'dalli'
 require 'perforated'
 require 'active_support/core_ext/object'
-require 'active_support/cache/dalli_store'
 require 'redis'
 require 'redis-activesupport'
 
@@ -36,13 +34,4 @@ Benchmark.bm do |x|
 
   x.report('redis-1') { perforated.to_json }
   x.report('redis-2') { perforated.to_json }
-
-  Perforated.configure do |config|
-    config.cache = ActiveSupport::Cache::DalliStore.new('localhost')
-  end
-
-  Perforated.cache.clear
-
-  x.report('dalli-1') { perforated.to_json }
-  x.report('dalli-2') { perforated.to_json }
 end
